acdb3f561f0371ae02fa96168f204a589f764508
AjayRaut1 <ajauraut2002@gmail.com>
2023-08-27 12:04:23 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,30 @@
+#Setup.py file will look for constructor file in src/mlprojects/components/_init_.py and install this folder as my local package
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-End-Wine-Prediction-ML-Model-MLflow-AWS"
+AUTHOR_USER_NAME = "ajayraut"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "coderajayraut@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
