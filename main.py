e840de195ad3480feacc25d762d41de40d5e10f4
AjayRaut1 <ajauraut2002@gmail.com>
2023-08-29 16:04:08 +0530

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 logger.info("Welcome to our Custom Logger (cause when deployed on ec2, we don't have access to python logs through the terminal)")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
